3667fb7088e3e885228f68449c98508f6d159389
Benjamin Tiddle <benjamin@intheflow.com>
2014-09-10 10:10:40 -0700

    Initialize repository with completed assignment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BlogPostMessage::Application.config.secret_key_base = 'd38497f2ad875438b3cdeb4fe7b69c09e9231d2684589aae625366792a3e022049c83568105f81f0d579f5b0ee4d476fa31f197c29439f0f5642fa986e64254c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BlogPostMessage::Application.config.session_store :cookie_store, key: '_blog-post-message_session'
